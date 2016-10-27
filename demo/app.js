0b136df9866dd2e5a3b9a5333ff93bbbcb772174
loujiahao <loujh@mizlicai.com>
2016-10-27 14:18:34 +0800

    初始化完成

---
+++ demo/app.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// const App = React.createClass({
+// 	render:function(){
+// 		return (
+// 			<div className=''>
+// 				asdf
+// 			</div>
+// 			)
+// 	}
+// })
+
+class App extends React.Component {
+	render(){
+		return (
+			<div className=''>
+				dfssdfs
+			</div>
+			)
+	}
+}
+
+ReactDOM.render(<App />,document.getElementById("app"))
